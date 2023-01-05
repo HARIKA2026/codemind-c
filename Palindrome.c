d1b5d44e0ddafa0c82f39ea33ab3f8f86268262b
HARIKA2026 <119848108+HARIKA2026@users.noreply.github.com>
2023-01-05 01:24:44 -0800

    #include<stdio.h> int main() {     int n,r,m,a,s=0;     scanf("%d",&a);     m=a;     while(m!=0)     {         r=m%10;         s=s*10+r;         m=m/10;     }     if(s==a)     printf("True");     else     {         printf("False");     } }

---
+++ Palindrome.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,r,m,a,s=0;
+    scanf("%d",&a);
+    m=a;
+    while(m!=0)
+    {
+        r=m%10;
+        s=s*10+r;
+        m=m/10;
+    }
+    if(s==a)
+    printf("True");
+    else
+    {
+        printf("False");
+    }
+}
